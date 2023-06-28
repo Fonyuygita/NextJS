e9bc4a61f482777c6145aef9ab5c0985559fff9f
Fonyuy Gita <FonyuyJudegita@gmail.com>
2023-06-28 06:10:01 -0700

    done with about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
 import styles from "./page.module.css"
+import Image from 'next/image'
+import Button from '@/component/button/Button'
 
 const About = () => {
   return (
-    <div className={styles.container}>About</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src='https://images.pexels.com/photos/1438072/pexels-photo-1438072.jpeg?auto=compress&cs=tinysrgb&w=600' fill={true} alt='lol' className={styles.img} />
+
+        <div className={styles.imageText}>
+          <h1 className={styles.imageTitle}>Students Story</h1>
+          <h2 className={styles.imageDesc}>Students who have benifited</h2>
+
+        </div>
+      </div>
+
+
+      <div className={styles.textContainer}>
+      <div className={styles.item}>
+      <h1 className={styles.title}>Who are We?</h1>
+      <p className={styles.desc}>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia rem ut et dolorum rerum nam aut assumenda neque deserunt eum, illo cumque velit, 
+      
+      <br/>
+      
+      repudiandae commodi quia ullam ad quam soluta. Porro vitae quia minus, in quae asperiores possimus inventore voluptate ad cum nisi? Distinctio, earum.
+
+      <br/>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, nemo quaerat omnis ex quia voluptatum minus a architecto perferendis, sit soluta ut officiis.
+      </p>
+
+      </div>
+
+<div className={styles.item}>
+
+<h1 className={styles.titl}>What We Do</h1>
+<p>
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos facere voluptate deleniti dolor rerum aliquid cumque voluptas fugit qui iste, nulla deserunt laboriosam molestiae expedita! Aliquid, alias possimus sit voluptate corporis dolorem nesciunt? Impedit veritatis possimus aspernatur laborum in fuga aut nemo, quas, illum qui explicabo iusto voluptatum exercitationem cum dicta earum maxime 
+<br/>
+
+<br/> - sapiente eveniet - incidunt? Nobis doloremque - facilis in possimus iusto
+</p>
+
+<Button text="Contacte" url="/portfolio"/>
+
+
+
+</div>
+
+      </div>
+
+    </div>
   )
 }
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,30 @@
 // import Image from 'next/image'
 import styles from './page.module.css'
 import React from 'react'
+import Hero from 'public/hero.png'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className={styles.container}>
+    <div className={styles.item}>
+    <h1 className={styles.title}>
+    Better Education For All
+    </h1>
+<p className={styles.desc}>
+Sharing your ideas with great minds to turn them to reality. Together we will learn, share and  grow.
+</p>
+<button className={styles.button}>See Details</button>
+
+    </div>
+
+    <div className={styles.item}>
+    
+    <Image src={Hero} alt='gita home'  />
+    
+    </div>
+    
+    </div>
   )
 }
 
