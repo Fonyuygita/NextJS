57f77575c2327965f40f447acb53495592f546e4
Fonyuy Gita <FonyuyJudegita@gmail.com>
2023-07-02 03:58:17 -0700

    connecting to database

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,25 +3,42 @@ import styles from "./page.module.css"
 import Image from 'next/image'
 import Link from 'next/link'
 
-const Blog = () => {
+
+
+const fetchData=async()=>{
+// fetch data here
+
+const res=await fetch("https://jsonplaceholder.typicode.com/posts")
+
+// check for possible errors before returning data
+if(!res.ok){
+  return notFound();
+}
+
+return res.json();
+}
+
+const Blog = async () => {
+// how to get the data here and use
+const data=await fetchData();
+console.log(data);
+
   return (
+
+
     <div className={styles.maincontainer}>
-    <Link href="/blog/:id" className={styles.Blog}>
+    {data.map((item)=>{
+        <Link href="/blog/:id" className={styles.Blog} key={item.id}>
     
     <div className={styles.imgContainer}>
-    <Image src="https://images.pexels.com/photos/5184951/pexels-photo-5184951.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="blog" width="400" height="250" className={styles.image} />
+    <Image src="https://images.pexels.com/photos/5184951/pexels-photo-5184951.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="blog" width="400" height="250" fill={true} className={styles.image} />
     
     </div>
     <div className={styles.content}>
-    <h1 className={styles.title}>first vlog</h1>
-    <p className={styles.desc}>welcome to my blog page</p>
-    
+    <h1 className={styles.title}>{item.title}</h1>
+    <p className={styles.desc}>{item.body}</p>
+</div>
+</Link>
+    })}
     </div>
-    
-    </Link>
-    
-    </div>
-  )
-}
-
-export default Blog
+    )}
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,8 +1,34 @@
+
+"use client"
 import React from 'react'
 import styles from "./page.module.css"
+import {useState, useEffect} from "react"
 
 
 const Dashboard = () => {
+
+  const [data, setData]=useState([]);
+
+  // fetch our data on the client side using seEffect
+  useEffect(()=>{
+
+const getData=async()=>{
+
+  const res=await fetch("https://jsonplaceholder.typicode.com/posts");
+  console.log(res)
+
+  if(!res.ok) return notFound();
+
+  // else we want to get our data back
+  const data =await res.json()
+  setData(data);
+}
+
+getData();
+
+  },[])
+
+  console.log(data)
   return (
     <div className={styles.container}>Dashboard</div>
   )
